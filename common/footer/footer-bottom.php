c1f2d4d85738e4159a2640b48ed901dd04bf0ff4
Nelson Amaya <nelsonamaya82@gmail.com>
2026-08-24 12:22:53 -0500

    Fix theme option names in footer and add escaping functions

--- common/footer/footer-bottom.php
+++ common/footer/footer-bottom.php
@@ -1,6 +1,6 @@
 <?php
 $hasFooterBottomLinks = false;
-if ( get_theme_option( 'footer_bottom_terms_link' ) || get_theme_option( 'footer_bottom_privacy_link' )) {
+if ( get_theme_option( 'footer_terms_link' ) || get_theme_option( 'footer_privacy_link' )) {
 	$hasFooterBottomLinks = true;
 }
 ?>
@@ -10,7 +10,7 @@ if ( get_theme_option( 'footer_bottom_terms_link' ) || get_theme_option( 'footer
 
         <!-- Copyright -->
         <div class="main-footer__copyright">
-            <?php if ( $copyright = get_theme_option( 'footer_bottom_copyright' ) ) : ?>
+            <?php if ( $copyright = get_theme_option( 'footer_copyright' ) ) : ?>
                 <?php echo $copyright; ?>
             <?php else: ?>
                 <?php echo $this->translate( 'Powered by Omeka S' ); ?>
@@ -19,24 +19,24 @@ if ( get_theme_option( 'footer_bottom_terms_link' ) || get_theme_option( 'footer
 
         <!-- Footer Bottom Links -->
         <div class="main-footer__links">
-            <?php if ( $terms_link = get_theme_option( 'footer_bottom_terms_link' ) ) : ?>
+            <?php if ( $terms_link = get_theme_option( 'footer_terms_link' ) ) : ?>
                 <?php
-                    $terms_title = get_theme_option( 'footer_bottom_terms_title' );
+                    $terms_title = get_theme_option( 'footer_terms_title' );
                     if ( !$terms_title ) {
                         $terms_title = $terms_link;
                     }
                 ?>
-                <a target="_blank" href="<?php echo $terms_link; ?>"><?php echo $terms_title; ?></a>
+                <a target="_blank" href="<?php echo html_escape($terms_link); ?>"><?php echo html_escape($terms_title); ?></a>
             <?php endif; ?>
 
-            <?php if ( $privacy_link = get_theme_option( 'footer_bottom_privacy_link' ) ) : ?>
+            <?php if ( $privacy_link = get_theme_option( 'footer_privacy_link' ) ) : ?>
                 <?php
-                    $privacy_title = get_theme_option( 'footer_bottom_privacy_title' );
+                    $privacy_title = get_theme_option( 'footer_privacy_title' );
                     if ( !$privacy_title ) {
                         $privacy_title = $privacy_link;
                     }
                 ?>
-                <a target="_blank" href="<?php echo $privacy_link; ?>"><?php echo $privacy_title; ?></a>
+                <a target="_blank" href="<?php echo html_escape($privacy_link); ?>"><?php echo html_escape($privacy_title); ?></a>
             <?php endif; ?>
         </div>
 
